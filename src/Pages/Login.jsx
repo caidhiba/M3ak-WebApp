4e913275bc7f08920b9475b4ed2a83ff7705c346
hiba <caidhiba05@gmail.com>
2025-04-08 23:44:04 +0100

    Revert "Merge pull request #3 from caidhiba/imane-therapy-feature"
    This reverts commit ad690b128962e061a7102ac1f7c7a5efb799347c, reversing
    changes made to 206e0494bcb2f04b26f8d004e05c522b60f3188c.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,103 +1,60 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../Styles/Login.css';
-import Header from "../Components/Header/Header";
-
-
-const Login = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Login submitted:', formData);
-    
-  };
-
-  return (
-    <div className="login-page">
-      <Header />
-      
-      <div className="login-content">
-        <div className="login-container">
-
-          {/* Left side - Login Form */}
-          <div className="login-form-container">
-            <div className="login-form-wrapper">
-              
-              <div className="form-container">
-                <h2 className="login-title">Log In</h2>
-                <form className="login-form" onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    <label>Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter your email"
-                      className="form-input"
-                      value={formData.email}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                  
-                  <div className="form-group">
-                    <label>Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Enter your password"
-                      className="form-input"
-                      value={formData.password}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                  
-                  <div className="forgot-password">
-                    <Link to="/forgot-password">Forgot password?</Link>
-                  </div>
-                  
-                  <button type="submit" className="login-button">
-                    Log In
-                  </button>
-                </form>
-                
-                <div className="divider">
-                  <span>Or</span>
-                </div>
-                
-                <div className="signup-link">
-                  Don't have an account? <Link to="/signup">Sign Up</Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          {/* Right side - Image */}
-          <div className="image-container">
-            <img
-              src="/src/assets/logIn.png"
-              alt="Login illustration"
-              className="cover-image"
-            />
-          </div>
-        </div>
-      </div>
-      
-      
-    </div>
-  );
-};
-
-export default Login;
+import React from 'react'; 
+import '../Styles/Login.css'; 
+ 
+const Login = () => { 
+return ( 
+<div className="login-container"> 
+{/* Logo in corner */} 
+<div className="logo"> 
+<img src="/src/assets/dark-logo.webp" alt="Logo" /> 
+</div> 
+{/* Left side - Login Form */} 
+<div className="login-form-container"> 
+<div className="login-form-wrapper"> 
+{/* Login Form */} 
+<div className="form-container"> 
+<h2 className="login-title">Log In</h2> 
+<form className="login-form"> 
+<input 
+type="email" 
+placeholder="Email" 
+className="form-input" 
+required 
+/> 
+<input 
+type="password" 
+placeholder="Password" 
+className="form-input" 
+required 
+/> 
+<button 
+type="submit" 
+className="login-button" 
+> 
+Log in 
+</button> 
+</form> 
+<div className="divider"></div> 
+<div className="signup-link"> 
+Don't have an account? <a href="/signup">Sign In</a> 
+</div> 
+</div> 
+{/* Copyright */} 
+<div className="copyright"> 
+© 2022 Relume 
+</div> 
+</div> 
+</div> 
+{/* Right side - Image */} 
+<div className="image-container"> 
+<img 
+src="/src/assets/logIn.png" 
+alt="logIn photo" 
+className="cover-image" 
+/> 
+</div> 
+</div> 
+); 
+}; 
+ 
+export default Login; 
