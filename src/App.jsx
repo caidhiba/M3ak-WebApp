63eabd5b62284f329a5a74b51d4b1c25bfac7af6
caidhiba <iman14092003@gmail.com>
2025-05-04 18:51:26 +0100

    Resolve the conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
-import BookShop from "./Pages/BookShop"
-import Header from "./Components/Header/Header"
-import Contact from "./Pages/Contact"; 
-import Login from "./Pages/Login"; 
-import SignIN from "./Pages/SignIn"; 
+import BookShop from "./Pages/BookShop";
+import Header from "./Components/Header/Header";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import SignIN from "./Pages/SignIn";
 import TherapistsList from "./Pages/TherapistsList";
 import PaymentPage from "./Pages/PaymentPage";
-<<<<<<< HEAD
 import Business from "./Pages/Business";
-
-=======
 import BookDetail from "./Components/BookDetail/BookDetail";
-import Recommendation from "./Pages/recommendation";
->>>>>>> a1dafa4bc3e8c60a10fad8e5dd9ce37d25214cd8
+//import Recommendation from "./Pages/recommendation";
+
 export default function App() {
   return (
     <Router>
@@ -27,17 +24,9 @@ export default function App() {
         <Route path="/login" element={<Login />} /> 
         <Route path="/signup" element={<SignIN />} /> 
         <Route path="/payment" element={<PaymentPage role="therapist" />} />
-<<<<<<< HEAD
-        <Route path="/Business" element={<Business/>} /> 
-=======
-
+       <Route path="/Business" element={<Business/>} /> 
         <Route path="/book/:id" element={<BookDetail />} />
-       
-
-
-      <Route path="/recommendation" element={<Recommendation />} />
-
->>>>>>> a1dafa4bc3e8c60a10fad8e5dd9ce37d25214cd8
+       {/*<Route path="/recommendation" element={<Recommendation />} />*/}
       </Routes>
     </Router>
   );
