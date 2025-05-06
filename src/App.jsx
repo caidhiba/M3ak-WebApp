4866bde684916b5c3a67c6bc31e7614de5cc7c89
houda2004 <nourelhoudahemmam@gmail.com>
2025-05-07 00:03:36 +0100

    go

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import BookDetail from "./Components/BookDetail/BookDetail";
 import Recommendation from "./Pages/recommendation";
 import { RoleProvider } from "./Pages/RoleContext";
 
+import VideoCall from "./Pages/InVideoCall";
+import ChatApp from "./Pages/ChatApp";
+import PrivateRoute from "./auth/PrivateRoute";
 //  const userRole = "therapist"
 // export default function App() {
  
@@ -36,10 +39,7 @@ import { RoleProvider } from "./Pages/RoleContext";
 //   );
 // }
 //import Recommendation from "./Pages/recommendation";
-import VideoCall from "./Pages/InVideoCall";
-import ChatApp from "./Pages/ChatApp";
 
-import PrivateRoute from "./auth/PrivateRoute";
 
 export default function App() {
   return (
@@ -57,26 +57,10 @@ export default function App() {
           <Route path="/Business" element={<Business />} />
           <Route path="/book/:id" element={<BookDetail />} />
           <Route path="/recommendation" element={<Recommendation />} />
+          <Route path="/VideoCall" element={<VideoCall />} />
+          <Route path="/MyContactes" element={<ChatApp />} />
         </Routes>
       </Router>
     </RoleProvider>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/therapists-list" element={<TherapistsList />} />
-        <Route path="/BookShop" element={<BookShop />} />
-        <Route path="/contact" element={<Contact />} /> 
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/signup" element={<SignIN />} /> 
-        <Route path="/payment" element={<PaymentPage role="therapist" />} />
-       <Route path="/Business" element={<Business/>} /> 
-        <Route path="/book/:id" element={<BookDetail />} />
-       {/*<Route path="/recommendation" element={<Recommendation />} />*/}
-
-       <Route path="/VideoCall" element={<VideoCall />} />
-       <Route path="/MyContactes" element={<ChatApp />} />
-      </Routes>
-    </Router>
   );
 }
