caba9097820ebd0b023fb11b7dc447435467b8a9
hiba <caidhiba05@gmail.com>
2025-05-16 14:20:23 +0100

    pulling

--- src/Pages/RoleContext.jsx
+++ src/Pages/RoleContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const RoleContext = createContext();
 
 export const RoleProvider = ({ children }) => {
-  const [role, setRole] = useState("client"); // you change the role here either client or therapist
+  const [role, setRole] = useState("therapist"); // you change the role here either client or therapist
 
   return (
     <RoleContext.Provider value={{ role, setRole }}>
